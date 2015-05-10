084af94562fadefd44316617355532fc273e56fd
Irvin Sandoval <irvandoval@gmail.com>
2015-05-10 00:46:50 -0600

    Agregados Atributos y metodos clase Usuario Creacion clase ControlDB

---
+++ app/src/main/java/com/example/irvandoval/reclamosgrupo17/ControlDB.java
@@ -0,0 +1,9 @@
+package com.example.irvandoval.reclamosgrupo17;
+
+/**
+ * Created by irvandoval on 05-10-15.
+ */
+public class ControlDB {
+    private static final String[] camposDetalleReclamo = new String[] {"id_detalle","id_prod_serv","descripcion_detalle"};
+
+}
--- app/src/main/java/com/example/irvandoval/reclamosgrupo17/usuario/Usuario.java
+++ app/src/main/java/com/example/irvandoval/reclamosgrupo17/usuario/Usuario.java
@@ -4,4 +4,77 @@ package com.example.irvandoval.reclamosgrupo17.usuario;
  * Created by irvandoval on 05-09-15.
  */
 public class Usuario {
+    private int  idUsuario;
+    private String nombreUsuario;
+    private String apellidoUsuario;
+    private String dui;
+    private String email;
+    private String telefono;
+    private int edad;
+
+    public Usuario(int idUsuario, String nombreUsuario, String apellidoUsuario, String dui, String email, String telefono, int edad) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.dui = dui;
+        this.email = email;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getApellidoUsuario() {
+        return apellidoUsuario;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setApellidoUsuario(String apellidoUsuario) {
+        this.apellidoUsuario = apellidoUsuario;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
