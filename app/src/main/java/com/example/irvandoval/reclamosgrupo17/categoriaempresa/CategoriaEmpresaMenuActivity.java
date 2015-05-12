1922e8a3cc3c27fdfc0cb571f3996f64234d7f30
Irvin Sandoval <irvandoval@gmail.com>
2015-05-12 16:57:22 -0600

    Mejora compatibilidad multiidioma

--- app/src/main/java/com/example/irvandoval/reclamosgrupo17/categoriaempresa/CategoriaEmpresaMenuActivity.java
+++ app/src/main/java/com/example/irvandoval/reclamosgrupo17/categoriaempresa/CategoriaEmpresaMenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.irvandoval.reclamosgrupo17.categoriaempresa;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,54 +16,38 @@ import android.widget.ListView;
 import com.example.irvandoval.reclamosgrupo17.R;
 
 public class CategoriaEmpresaMenuActivity extends ListActivity {
-    String[] menu={"Insertar Registro","Eliminar Registro","Consultar Registro", "ActualizarRegistro"};
+    String[] menu;
             String[] activities={"CategoriaEmpresaInsertarActivity","CategoriaEmpresaEliminarActivity","CategoriaEmpresaConsultarActivity",
             "CategoriaEmpresaActualizarActivity"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_categoria_empresa_menu);
         ListView listView = getListView();
         listView.setBackgroundColor(Color.rgb(0, 0, 255));
-        ArrayAdapter<String> adapter= new  ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setMenu(getResources());
+        ArrayAdapter<String> adapter = new  ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
         setListAdapter(adapter);
 
     }
 
 
     @Override
-   /* public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_categoria_empresa_menu, menu);
-        return true;
-    }*/
-    protected void onListItemClick(ListView l,View v,int position,long id){
+    protected void onListItemClick(ListView l,View v, int position, long id){
         super.onListItemClick(l, v, position, id);
-        String nombreValue=activities[position];
+        String nombreValue = activities[position];
         l.getChildAt(position).setBackgroundColor(Color.rgb(128, 128, 255));
         try{
-            Class<?>
-                    clase=Class.forName("com.example.irvandoval.reclamosgrupo17.categoriaempresa."+nombreValue);
+            Class<?> clase=Class.forName("com.example.irvandoval.reclamosgrupo17.categoriaempresa."+nombreValue);
             Intent inte = new Intent(this,clase);
             this.startActivity(inte);
         }catch(ClassNotFoundException e){
             e.printStackTrace();
         }
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void setMenu(Resources res){
+        menu = new String[] {res.getString(R.string.insertar),res.getString(R.string.eliminar)
+                ,res.getString(R.string.consultar),res.getString(R.string.actualizar)};
     }
+
 }
