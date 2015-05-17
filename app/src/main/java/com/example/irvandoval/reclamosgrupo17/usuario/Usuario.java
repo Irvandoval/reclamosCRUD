c0ccbe38659b18775a4d21ffb0f9b7e06d7d0d7a
Irvin Sandoval <irvandoval@gmail.com>
2015-05-16 22:49:26 -0600

    Agregado Sexo como campo de Usuario

--- app/src/main/java/com/example/irvandoval/reclamosgrupo17/usuario/Usuario.java
+++ app/src/main/java/com/example/irvandoval/reclamosgrupo17/usuario/Usuario.java
@@ -11,62 +11,71 @@ public class Usuario {
     private String email;
     private String telefono;
     private int edad;
+    private String sexo;
 
     public Usuario() {
     }
 
-    public Usuario(String nombreUsuario, String apellidoUsuario, String dui, String email, String telefono, int edad) {
-       // this.idUsuario = idUsuario;
+    public Usuario(String nombreUsuario, String apellidoUsuario, String dui, String email, String telefono, int edad, String sexo) {
         this.nombreUsuario = nombreUsuario;
         this.apellidoUsuario = apellidoUsuario;
         this.dui = dui;
         this.email = email;
         this.telefono = telefono;
         this.edad = edad;
+        this.sexo = sexo;
     }
 
     public String getNombreUsuario() {
         return nombreUsuario;
     }
 
-    public String getApellidoUsuario() {
-        return apellidoUsuario;
-    }
-
-    public String getDui() {
-        return dui;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
     public void setNombreUsuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getApellidoUsuario() {
+        return apellidoUsuario;
     }
 
     public void setApellidoUsuario(String apellidoUsuario) {
         this.apellidoUsuario = apellidoUsuario;
     }
 
-    public void setDui(String dui) {
-        this.dui = dui;
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public int getEdad() {
+        return edad;
     }
 
     public void setEdad(int edad) {
